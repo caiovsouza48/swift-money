f4a694d601d9b9a53ce71095cb383160ab4a6d4a
caiovsouza48 <caio.vsouza48@gmail.com>
2025-05-31 19:43:26 -0300

    Rename Money workspace to MonetaryValue for Xcode compatibility issue with the Money struct which causes conflict

--- Package.swift
+++ Package.swift
@@ -13,13 +13,13 @@ let package = Package(
         .visionOS(.v1),
     ],
     products: [
-        .library(name: "Money", targets: ["Money"]),
+        .library(name: "MonetaryValue", targets: ["MonetaryValue"]),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.4.3"),
     ],
     targets: [
-        .target(name: "Money"),
-        .testTarget(name: "MoneyTests", dependencies: ["Money"]),
+        .target(name: "MonetaryValue"),
+        .testTarget(name: "MoneyTests", dependencies: ["MonetaryValue"]),
     ]
 )
